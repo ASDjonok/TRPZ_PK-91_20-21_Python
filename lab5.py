8055e0ee78f10e412f170019613d2963f23339b6
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2020-12-15 12:07:16 +0200

    add lab5.py

---
+++ lab5.py
@@ -0,0 +1,101 @@
+class Text:
+    # def __init__(self, text_string):
+    def __init__(self, sentences):
+        # pass
+        # self.text_string = text_string
+        self.sentences = sentences
+
+    def __str__(self):
+        # return self.sentences
+        return ''.join(map(str, self.sentences))
+
+
+class Sentence:
+    # def __init__(self, word_string="", letters=[]):
+    def __init__(self, sentence_elements):
+        # pass
+        # self.text_string = word_string
+        self.sentence_elements = sentence_elements
+
+    def __str__(self):
+        # return self.text_string + self.letters
+        # return self.text_string + ' ' + ''.join(map(str, self.letters))
+        sentence_string = ""
+        for sentence_element in self.sentence_elements:
+            if isinstance(sentence_element, Word):
+                sentence_string += '' + str(sentence_element) + ' '
+            else:
+                sentence_string += '' + str(sentence_element)
+
+        # return ' '.join(map(str, self.sentence_elements))
+        return sentence_string
+
+
+class Word:
+    # def __init__(self, word_string="", letters=[]):
+    def __init__(self, letters):
+        # pass
+        # self.text_string = word_string
+        self.letters = letters
+
+    def __str__(self):
+        # return self.text_string + self.letters
+        # return self.text_string + ' ' + ''.join(map(str, self.letters))
+        return ''.join(map(str, self.letters))
+
+
+class Punctuation:
+    def __init__(self, symbols):
+        # pass
+        self.symbols = symbols
+
+    def __str__(self):
+        return self.symbols
+
+
+class Letter:
+    def __init__(self, symbol):
+        # pass
+        self.symbol = symbol
+
+    def __str__(self):
+        return self.symbol
+
+
+if __name__ == '__main__':
+    pass
+
+# text = Text("Hello world")
+# Hello, world! I am Program?
+
+
+letter = Letter('a')
+letter2 = Letter('b')
+# word = Word("ab", [letter, letter2])
+word = Word([letter, letter2])
+# myText.myCustomField = "1";
+
+print(Text([
+    Sentence([
+        Word([
+            Letter('H'),
+            Letter('e'),
+            Letter('l'),
+            Letter('l'),
+            Letter('o'),
+        ]),
+        Punctuation(','),
+        Word([
+            Letter('w'),
+            Letter('o'),
+            Letter('r'),
+            Letter('l'),
+            Letter('d'),
+        ]),
+        Punctuation('!'),
+    ]),
+    # Sentence(),
+]))
+print(letter)
+print(word)
+# print(myText.myCustomField)
